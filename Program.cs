5906954fe5e935256dcd0e1430bbf23305e47587
Metritutus <324345+Metritutus@users.noreply.github.com>
2021-11-08 20:51:38 +0000

    Added code to test Rotate() execution time with different resamplers.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -12,20 +14,44 @@ namespace ImageSharpRotateTest001
         {
             var bytes = File.ReadAllBytes("Images\\TestImage.png");
 
-            RunWithStopWatch(() => RotateImageWithImageSharp(bytes, 10), $"RotateImageWithImageSharp(){Environment.NewLine}", "RotateImageWithImageSharp() completed");
+            List<(string Name, IResampler Resampler)> resamplers = new List<(string Name, IResampler Resampler)>()
+            {
+                (nameof(KnownResamplers.Bicubic), KnownResamplers.Bicubic),
+                (nameof(KnownResamplers.Box), KnownResamplers.Box),
+                (nameof(KnownResamplers.CatmullRom), KnownResamplers.CatmullRom),
+                (nameof(KnownResamplers.Hermite), KnownResamplers.Hermite),
+                (nameof(KnownResamplers.Lanczos2), KnownResamplers.Lanczos2),
+                (nameof(KnownResamplers.Lanczos3), KnownResamplers.Lanczos3),
+                (nameof(KnownResamplers.Lanczos5), KnownResamplers.Lanczos5),
+                (nameof(KnownResamplers.Lanczos8), KnownResamplers.Lanczos8),
+                (nameof(KnownResamplers.MitchellNetravali), KnownResamplers.MitchellNetravali),
+                (nameof(KnownResamplers.NearestNeighbor), KnownResamplers.NearestNeighbor),
+                (nameof(KnownResamplers.Robidoux), KnownResamplers.Robidoux),
+                (nameof(KnownResamplers.RobidouxSharp), KnownResamplers.RobidouxSharp),
+                (nameof(KnownResamplers.Spline), KnownResamplers.Spline),
+                (nameof(KnownResamplers.Triangle), KnownResamplers.Triangle),
+                (nameof(KnownResamplers.Welch), KnownResamplers.Welch)
+            };
+
+
+            foreach(var resampler in resamplers)
+            {
+                RunWithStopWatch(() => RotateImageWithImageSharp(bytes, 10, resampler.Resampler), $"RotateImageWithImageSharp() - resampler {resampler.Name}{Environment.NewLine}", "RotateImageWithImageSharp() completed");
+            }
+
             Console.WriteLine();
             RunWithStopWatch(() => RotateImageWithSystemDrawing(bytes, 10), $"RotateImageWithSystemDrawing(){Environment.NewLine}", "RotateImageWithSystemDrawing() completed");
 
             Console.WriteLine("Done.");
         }
 
-        public static byte[] RotateImageWithImageSharp(byte[] imageBytes, float angleDegrees)
+        public static byte[] RotateImageWithImageSharp(byte[] imageBytes, float angleDegrees, IResampler resampler)
         {
             using MemoryStream inputStream = RunWithStopWatch(() => new MemoryStream(imageBytes), "Initialising input stream.", "Input stream initialised");
             using Image image = RunWithStopWatch(() => Image.Load(inputStream), "Loading image.", "Image loaded");
             using MemoryStream outputStream = new MemoryStream();
 
-            RunWithStopWatch(() => image.Mutate(i => i.Rotate((int)angleDegrees)), "Rotating.", "Rotation completed");
+            RunWithStopWatch(() => image.Mutate(i => i.Rotate((int)angleDegrees, resampler)), "Rotating.", "Rotation completed");
             RunWithStopWatch(() => image.SaveAsPng(outputStream), "Saving as png to stream.", "Save completed");
 
             return outputStream.ToArray();
